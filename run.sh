787d7865d6a8bf7d0af51fcd4e91fbf5ec8c742f
Phi Lam <lamyenphi14@gmail.com>
2025-04-12 11:37:16 -0700

    FID scores during training under 40

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 python pcnn_train.py \
 --batch_size 16 \
 --sample_batch_size 32 \
---sampling_interval 20 \
---save_interval 20 \
+--sampling_interval 15 \
+--save_interval 15 \
 --dataset cpen455 \
 --nr_resnet 1 \
---nr_filters 40 \
+--nr_filters 60 \
 --nr_logistic_mix 5 \
 --lr_decay 0.999995 \
 --max_epochs 200 \
