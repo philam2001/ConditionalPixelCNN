fef0f25b5233dc2da490af7a21e7c4c3e6faef69
Phi Lam <lamyenphi14@gmail.com>
2025-04-15 15:04:12 -0700

    from sweep

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 python pcnn_train.py \
 --batch_size 16 \
---sample_batch_size 8 \
+--sample_batch_size 16 \
 --sampling_interval 25 \
 --save_interval 25 \
 --dataset cpen455 \
 --nr_resnet 2 \
---nr_filters 60 \
+--nr_filters 80 \
 --nr_logistic_mix 10 \
 --lr_decay 0.999995 \
 --max_epochs 200 \
